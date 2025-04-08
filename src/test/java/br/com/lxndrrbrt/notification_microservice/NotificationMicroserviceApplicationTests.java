f21139bf94d1243c9bcbf23632203cc67b2b5772
aleroberto <alexandreroberto2@gmail.com>
2025-04-08 16:52:03 -0300

    Project setup with Spring Boot

---
+++ src/test/java/br/com/lxndrrbrt/notification_microservice/NotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lxndrrbrt.notification_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
